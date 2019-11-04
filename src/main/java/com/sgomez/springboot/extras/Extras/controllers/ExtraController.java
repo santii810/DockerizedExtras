4bfc1862fbcf75f32f034b41f0bbb8e0b725a754
Santiago Gómez Vilar <sgomez@optaresolutions.com>
2019-11-04 14:22:22 +0100

    Completes extras CRUD

--- src/main/java/com/sgomez/springboot/extras/Extras/controllers/ExtraController.java
+++ src/main/java/com/sgomez/springboot/extras/Extras/controllers/ExtraController.java
@@ -1,13 +1,16 @@
 package com.sgomez.springboot.extras.Extras.controllers;
 
+import com.sgomez.springboot.extras.Extras.exceptions.ResourceNotFoundException;
 import com.sgomez.springboot.extras.Extras.models.ExtraEntity;
 import com.sgomez.springboot.extras.Extras.services.ExtraService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.MediaType;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
+import javax.validation.Valid;
+import java.net.URI;
 import java.util.List;
 
 @RestController
@@ -15,13 +18,39 @@ public class ExtraController {
     @Autowired
     private ExtraService extraService;
 
-    @RequestMapping(
-            value = "extras",
-            method = RequestMethod.GET,
-            produces = MediaType.APPLICATION_JSON_VALUE
-    )
+    @GetMapping("extras")
     public List<ExtraEntity> getAll() {
         return extraService.findAll();
     }
 
+
+    @GetMapping("/extra/{id}")
+    public ExtraEntity retrieveStudent(@PathVariable int id) {
+        ExtraEntity extra = extraService.findById(id)
+                .orElseThrow(() -> new ResourceNotFoundException("Extra", "id", id));
+        return extra;
+    }
+
+    @PostMapping("extra")
+    public ResponseEntity<?> save(ExtraEntity extra) {
+        ExtraEntity savedExtra = extraService.save(extra);
+
+        URI location = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}")
+                .buildAndExpand(savedExtra.getId()).toUri();
+        return new ResponseEntity<>(location.toString(), HttpStatus.CREATED);
+    }
+
+
+    @DeleteMapping("extra/{id}")
+    public ExtraEntity delete(@PathVariable("id") int extra) {
+        return extraService.delete(extra);
+    }
+
+
+    //    @PutMapping("/extra/{id}")
+    @RequestMapping(value = "/extra/{id}", method = RequestMethod.PUT, produces = "application/json", consumes = "application/json")
+    public ExtraEntity updateNote(@PathVariable(value = "id") int id,
+                                  @Valid @RequestBody ExtraEntity details) {
+        return extraService.update(id, details);
+    }
 }
--- src/main/java/com/sgomez/springboot/extras/Extras/services/ExtraService.java
+++ src/main/java/com/sgomez/springboot/extras/Extras/services/ExtraService.java
@@ -1,21 +1,73 @@
 package com.sgomez.springboot.extras.Extras.services;
 
+import com.sgomez.springboot.extras.Extras.exceptions.ResourceNotFoundException;
 import com.sgomez.springboot.extras.Extras.models.ExtraEntity;
+import com.sgomez.springboot.extras.Extras.models.LocalEntity;
+import com.sgomez.springboot.extras.Extras.models.UserEntity;
 import com.sgomez.springboot.extras.Extras.repositories.ExtraRepository;
+import com.sgomez.springboot.extras.Extras.repositories.LocalRepository;
+import com.sgomez.springboot.extras.Extras.repositories.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
+import org.springframework.web.server.ResponseStatusException;
 
 import java.util.List;
+import java.util.Optional;
 
 
 @Service
 public class ExtraService {
     @Autowired
     ExtraRepository extraRepository;
+    @Autowired
+    LocalRepository localRepository;
+    @Autowired
+    UserRepository userRepository;
 
     public List<ExtraEntity> findAll() {
         return extraRepository.findAll();
     }
 
+    public ExtraEntity save(ExtraEntity extra) {
+        return extraRepository.save(extra);
+    }
 
+    public ExtraEntity delete(Integer id) {
+        Optional<ExtraEntity> extra = extraRepository.findById(id);
+
+        if (!extra.isPresent()) {
+            throw new ResponseStatusException(
+                    HttpStatus.NOT_FOUND, "Dont found extra with id = " + id
+            );
+        }
+        extraRepository.delete(extra.get());
+        return extra.get();
+    }
+
+
+    public Optional<ExtraEntity> findById(int id) {
+        return extraRepository.findById(id);
+    }
+
+    public ExtraEntity update(int id, ExtraEntity details) {
+        ExtraEntity extra = extraRepository.findById(id)
+                .orElseThrow(() -> new ResourceNotFoundException("Extra", "id", id));
+
+        if (details.getUser() != null) {
+            int userId = details.getUser().getId();
+            UserEntity user = userRepository.findById(userId)
+                    .orElseThrow(() -> new ResourceNotFoundException("User", "id", userId));
+            extra.setUser(user);
+        }
+        if (details.getLocal() != null) {
+            int localId = details.getLocal().getId();
+            LocalEntity local = localRepository.findById(localId)
+                    .orElseThrow(() -> new ResourceNotFoundException("Local", "id", localId));
+            extra.setLocal(local);
+        }
+
+        ExtraEntity updatedExtra = extraRepository.save(extra);
+        return updatedExtra;
+    }
 }
